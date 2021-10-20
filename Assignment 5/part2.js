7fb6fdb5afad8c93a692553c5e737cf2032a3584
phxsrp <68784379+phxsrp@users.noreply.github.com>
2021-10-20 17:00:26 +0700

    part2 html json

---
+++ Assignment 5/part2.js
@@ -0,0 +1,10 @@
+import {getWord} from "./part1.js";
+// import getWord จาก part1.js
+
+
+// --- ทดลองเรียกใช้ function getWord ---
+let censorWord = getWord(['jajah', 'aom', 'arm','phop']);
+console.log(censorWord('jah'));
+console.log(censorWord('jajah'));
+console.log(censorWord('phop'));
+
